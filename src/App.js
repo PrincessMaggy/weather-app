853dcaff5c52638357265c6cab5738bf62de73ba
PrincessMaggy <edoziemagdalene@gmail.com>
2022-08-16 21:06:37 +0100

    fetched data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React,{useEffect, useState} from 'react';
+
 
 function App() {
-  return (
+  const [weather, setWeather] = useState(null);
+  const [input, setInput] = useState("");
+
+  useEffect(()=>{
+   axios.get("http://api.weatherapi.com/v1/current.json?key=44d4692372204a37ab8140704221608&q=London")
+    .then((data) =>{
+      setWeather(data.data);
+    })
+    .catch((err)=> console.log(err));
+  },[]);
+const weatherInput =(e) =>{
+  setInput(e.target.value);
+}
+const searchWeather=()=>{
+  axios.get(`http://api.weatherapi.com/v1/current.json?key=44d4692372204a37ab8140704221608&q=${input}`)
+  .then(data =>{
+    setWeather(data.data);
+  })  
+}
+
+ return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {weather && (
+        <div>
+          <div className='search'>
+            <input type="text" onChange={weatherInput} />
+            <button onClick={searchWeather}>SEARCH</button>
+          </div>
+          <div className='weatherInfo'>
+            <h1>{weather.location.country}</h1>
+            <h2>{weather.location.region}</h2>
+            <div className='condition'>
+              <h3>{weather.current.condition.text}</h3>
+              <img  src={weather.current.condition.icon} alt="icon"/>
+              <h3>{weather.current.temp_c}deg Celsius</h3>
+              </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
